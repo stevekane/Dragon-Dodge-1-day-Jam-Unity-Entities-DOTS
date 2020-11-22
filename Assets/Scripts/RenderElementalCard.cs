7c20760df9054e1abd7a3b74acf836990cac2a01
Steve Kane <kanesteven@gmail.com>
2020-11-22 16:45:46 -0600

    added stubbed in layout for player 1 hand

---
+++ Assets/Scripts/RenderElementalCard.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class RenderElementalCard : MonoBehaviour {
+  public MeshRenderer ElementalMeshRenderer;
+
+  public void SetElementalMaterial(RenderGameObjects renderGameObjects, in Element element) {
+    switch (element) {
+    case Element.Earth:
+    ElementalMeshRenderer.material = renderGameObjects.EarthMaterial;
+    break;
+
+    case Element.Fire:
+    ElementalMeshRenderer.material = renderGameObjects.FireMaterial;
+    break;
+
+    case Element.Wind:
+    ElementalMeshRenderer.material = renderGameObjects.WindMaterial;
+    break;
+
+    case Element.Water:
+    ElementalMeshRenderer.material = renderGameObjects.WaterMaterial;
+    break;
+    }
+  }
+}
---
+++ Assets/Scripts/RenderSpellCard.cs
@@ -0,0 +1,5 @@
+﻿using UnityEngine;
+
+public class RenderSpellCard : MonoBehaviour {
+  public MeshRenderer SpellMeshRenderer;
+}
